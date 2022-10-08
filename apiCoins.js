bab85999d319c2af7aee3bfe7c3b158b5da6430e
Joey Hetfield <joeydefenix@hotmail.com>
2022-10-08 19:17:36 -0300

    Requisito 1 + ordem certa do map

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,15 @@
+
 const fetchCoins = async () => {
+  try {
+    const url = 'https://api.coincap.io/v2/assets';
+    const response = await fetch(url);
+    const jsonResponse = await response.json();
+    const perfectResponse = jsonResponse.data.map((moeda) => `${moeda.name} ${moeda.symbol}: ${moeda.priceUsd}`)
+    console.log(perfectResponse)
+  }
+  catch(error) {
+    console.log('Algo deu errado', error)
+  }
 };
 
 fetchCoins();
